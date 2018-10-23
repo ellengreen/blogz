96afc4ddbaaf84ff63a08cc5244a2f4c49a20963
Ellen Green <eegreen.95@gmail.com>
2018-10-23 16:34:12 -0500

    Finished signup/login templates

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app) 
 #secret key
 
-#Blog and User Classes
+
+
 class Blog(db.Model):
+
     id = db.Column(db.Integer,primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(120))
@@ -22,8 +24,9 @@ class Blog(db.Model):
         self.owner_id = owner
 
 class User(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20))
+    username = db.Column(db.String(30), unique=True)
     password = db.Column(db.String(20))
     blogs = db.relationship('Blog', backref='owner')
 
@@ -31,13 +34,15 @@ class User(db.Model):
         self.username = username
         self.password = password
 
-#Home Page Route
+
+
 @app.route('/')
 def index():
     entries = Blog.query.order_by(desc(Blog.id)).all()
     return render_template('blog.html', entries=entries)
 
-#View Single Post Route
+
+
 @app.route('/blog')
 def post_id():
     id = request.args.get('id')
@@ -49,7 +54,8 @@ def post_id():
         print(single_post.title,single_post.body)
     return render_template('single_post.html', single_post=single_post)
 
-#Add New Post Route
+
+
 @app.route('/newpost', methods=['POST', 'GET'])
 def new_post():
 
@@ -75,20 +81,55 @@ def new_post():
     
     return render_template('newpost.html')
 
+
+
 @app.before_request
 def require_login():
     allowed_routes = ['login', 'signup', 'blog', 'single_post', 'index']
     if request.endpoint not in allowed_routes and 'username' not in session:
         return redirect('/login')
 
-#Login Route
+
+
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        
+        user = User.query.filter_by(username=username).first()
 
-#Run App
+        if user and user.password == password:
+            session['username'] = username
+            return redirect ('/newpost')
+        
+    return render_template('login.html')
+
+
+
+@app.route('/signup')
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+    
+        existing_user = User.query.filter_by(username=username).first()
+        if not existing_user:
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            return redirect('/')
+    
+    return render_template('signup.html')
+
+
+
+@app.route('/logout')
+def logout():
+    del session['username']
+    return redirect('/')
+
+
 if __name__ == '__main__':
     app.run()
