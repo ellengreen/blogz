690e78cdba3cc1c00c032a7e93d1bf674c3765c8
Ellen Green <eegreen.95@gmail.com>
2018-10-23 16:00:36 -0500

    added new template files

--- main.py
+++ main.py
@@ -4,25 +4,40 @@ from sqlalchemy import desc
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:ellen@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:ellen@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app) 
+#secret key
 
+#Blog and User Classes
 class Blog(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(120))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __init__(self,title,body):
         self.title = title
         self.body = body
+        self.owner_id = owner
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20))
+    password = db.Column(db.String(20))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+#Home Page Route
 @app.route('/')
 def index():
     entries = Blog.query.order_by(desc(Blog.id)).all()
-    #return redirect('/blog')
     return render_template('blog.html', entries=entries)
 
+#View Single Post Route
 @app.route('/blog')
 def post_id():
     id = request.args.get('id')
@@ -34,6 +49,7 @@ def post_id():
         print(single_post.title,single_post.body)
     return render_template('single_post.html', single_post=single_post)
 
+#Add New Post Route
 @app.route('/newpost', methods=['POST', 'GET'])
 def new_post():
 
@@ -59,6 +75,20 @@ def new_post():
     
     return render_template('newpost.html')
 
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'signup', 'blog', 'single_post', 'index']
+    if request.endpoint not in allowed_routes and 'username' not in session:
+        return redirect('/login')
 
+#Login Route
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+
+#Run App
 if __name__ == '__main__':
     app.run()
